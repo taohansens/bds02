2022f005cb2e338a6f2859dc630e779e8daa16bf
taohansens <taohansens@gmail.com>
2023-04-17 20:33:50 -0300

    findAllShouldReturnAllResourcesSortedByName. [Pass]

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -1,8 +1,17 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
@@ -10,5 +19,11 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
-
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll();
+        return list.stream().map(CityDTO::new)
+                .sorted(Comparator.comparing(CityDTO::getName))
+                .collect(Collectors.toList());
+    }
 }
